6a420b9dc305ea5de7c8d12a0db4988333ef51a3
Ayush Sharma <aayush240204@gmail.com>
2020-12-08 19:00:34 +0530

    Init Push

---
+++ plugins/start.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@Client.on_message(Filters.command(["start"]), group=-2)
+async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Channel❤️", url="https://t.me/AyushBots")],
+        [InlineKeyboardButton(
+            "Report Bugs 😊", url="https://t.me/CyberBoyAyushBot")]
+    ])
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    await message.reply_text(welcomed, reply_markup=joinButton)
+    raise StopPropagation
