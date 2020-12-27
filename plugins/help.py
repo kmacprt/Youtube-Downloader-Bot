49a9ba38e08e64d69ebcf5883a81f827df4d866f
kmacprt <72274248+kmacprt@users.noreply.github.com>
2020-12-27 21:43:19 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url \nJoin @AyushBots🤗️"
+    helptxt = f"දැනට සහය දක්වන්නේ යූටියුබ් සිංගල් සඳහා පමණි (ධාවන ලැයිස්තුවක් නොමැත) යූටියුබ් යූආර්එල් යවන්න \nසම්බන්ධ කරගන්න @kavinduaj🤗️"
     await message.reply_text(helptxt)
